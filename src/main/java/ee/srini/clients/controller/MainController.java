235343662189af522eea0b4ddfe72b8fdfdf3275
Anastassia Sinkova <anastassias@proekspert.ee>
2020-08-27 12:41:39 +0300

    Error page added, clean-up

--- src/main/java/ee/srini/clients/controller/MainController.java
+++ src/main/java/ee/srini/clients/controller/MainController.java
@@ -8,10 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -67,5 +64,14 @@ public class MainController {
         model.addAttribute("message", message);
 
         return "index";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleError(Exception e, Model model) {
+        log.debug("Handling exception: {}", e.getMessage());
+
+        model.addAttribute("exception", e);
+
+        return "error";
     }
 }
